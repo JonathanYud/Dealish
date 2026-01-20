a1bd097c004d824a6b50ab89d7a40b25a5256720
Jonathan Yudhistira <jonathanyud@gmail.com>
2026-01-20 11:23:49 -0500

    Added second deal to test columns

--- src/app/data/deals.js
+++ src/app/data/deals.js
@@ -3,11 +3,20 @@ export const deals = [
     id: 1,
     title: "BOGO Free Pizza",
     restaurant: "Joe’s Slices",
-    start_time: "12 PM",
-    end_time: "2 PM",
-    ends_in: "Ends in 3h 12m",
+    start_time: new Date("2026-01-30T12:00:00"),
+    end_time: new Date("2026-01-30T14:00:00"),
     description: "Free pizza with order of one",
     likes: 243,
     image: "/pizza.jpg",
   },
+  {
+    id: 2,
+    title: "Wing Night",
+    restaurant: "Big Wings",
+    start_time: new Date("2026-01-30T17:00:00"),
+    end_time: new Date("2026-01-30T22:00:00"),
+    description: "$0.50 wings",
+    likes: 1000,
+    image: "",
+  }
 ]
