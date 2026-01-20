8cb190ddb845e8e2ec5bb99be8e67000b0f0139a
Jonathan Yudhistira <jonathanyud@gmail.com>
2026-01-20 11:24:33 -0500

    Changed column count smaller for mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       </header>
 
       <main className="max-w-8xl mx-auto p-4">
-        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md: grid-cols-3 lg:grid-cols-4">
+        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md: grid-cols-1 lg:grid-cols-4">
           {deals.map(deal => (
             <DealCard key={deal.id} deal={deal} />
           ))}
