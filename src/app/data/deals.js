6e008fd86680c7bb6a8f9e2720102be28b8a1c88
Jonathan Yudhistira <jonathanyud@gmail.com>
2026-01-20 11:33:01 -0500

    Added a status indictor based on the time the deal is running

--- src/app/data/deals.js
+++ src/app/data/deals.js
@@ -1,3 +1,13 @@
+const now = new Date();
+
+function getStatus(start, end) {
+  const startTime = new Date(start);
+  const endTime = new Date(end);
+  if (now < startTime) return 'upcoming';
+  if (now > endTime) return 'past';
+  return 'running';
+}
+
 export const deals = [
   {
     id: 1,
@@ -5,6 +15,7 @@ export const deals = [
     restaurant: "Joe’s Slices",
     start_time: new Date("2026-01-30T12:00:00"),
     end_time: new Date("2026-01-30T14:00:00"),
+    status: getStatus(new Date("2026-01-30T12:00:00"), new Date("2026-01-30T14:00:00")),
     description: "Free pizza with order of one",
     likes: 243,
     image: "/pizza.jpg",
@@ -15,8 +26,9 @@ export const deals = [
     restaurant: "Big Wings",
     start_time: new Date("2026-01-30T17:00:00"),
     end_time: new Date("2026-01-30T22:00:00"),
+    status: getStatus(new Date("2026-01-30T17:00:00"), new Date("2026-01-30T22:00:00")),
     description: "$0.50 wings",
     likes: 1000,
-    image: "",
+    image: null,
   }
 ]
