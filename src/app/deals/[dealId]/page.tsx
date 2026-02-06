79f75b99ba884187417451a13afd5f75711e8fcf
Jonathan Yudhistira <jonathanyud@gmail.com>
2026-02-05 21:15:17 -0500

    Added page for individual deals

---
+++ src/app/deals/[dealId]/page.tsx
@@ -0,0 +1,12 @@
+import { deals  } from "../../data/deals";
+
+export default function DealPage() {
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <header className="p-4 bg-white shadow flex justify-beginning">
+            <img src="/logo2.png" alt="Dealish Logo" className="h-10 w-auto" />
+            </header>
+
+        </div>
+    );
+}
